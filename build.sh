9ecc8a0e95addd23e41b458575ab5b274b75cda6
gityzon <109445070+gityzon@users.noreply.github.com>
2022-09-04 22:51:03 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -50,13 +50,13 @@ server {
             proxy_http_version                  1.1;
 
             # 指定头部：
-            proxy_set_header  Upgrade           $http_upgrade;
+            proxy_set_header  Upgrade           \$http_upgrade;
             proxy_set_header  Connection        "upgrade";
-            proxy_set_header  Host              $http_host;
+            proxy_set_header  Host              \$http_host;
             proxy_read_timeout  300s;
             # Show realip in access.log
-            proxy_set_header  X-Real-IP         $remote_addr;
-            proxy_set_header  X-Forwarded-For   $proxy_add_x_forwarded_for;
+            proxy_set_header  X-Real-IP         \$remote_addr;
+            proxy_set_header  X-Forwarded-For   \$proxy_add_x_forwarded_for;
         }
 
 
@@ -69,13 +69,13 @@ server {
             proxy_http_version                  1.1;
 
             # 指定头部：
-            proxy_set_header  Upgrade           $http_upgrade;
+            proxy_set_header  Upgrade           \$http_upgrade;
             proxy_set_header  Connection        "upgrade";
-            proxy_set_header  Host              $http_host;
+            proxy_set_header  Host              \$http_host;
             proxy_read_timeout  300s;
             # Show realip in access.log
-            proxy_set_header  X-Real-IP         $remote_addr;
-            proxy_set_header  X-Forwarded-For   $proxy_add_x_forwarded_for;
+            proxy_set_header  X-Real-IP         \$remote_addr;
+            proxy_set_header  X-Forwarded-For   \$proxy_add_x_forwarded_for;
         }
 
     location /$password {
